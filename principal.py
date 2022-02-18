608266e44de5b2bc78004e449e314a49349030c2
Rhyan Jackson <rhyanjacksonmarinho@gmail.com>
2022-02-17 23:15:21 -0300

    Correção de horário.
    API de horário de Brasília implementada.
    Antigo problema: Script puxava horário da OS, não vinha com o horário de Brasília.

--- principal.py
+++ principal.py
@@ -1,6 +1,5 @@
 import json
 import requests as re
-import datetime
 try:
     api = re.get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL')
     api = json.loads(api.text)
@@ -8,9 +7,13 @@ except Exception as erro:
     print('Ocorreu um erro com sua requisição. Tente novamente.\n'
           f'Código de erro: {erro}.')
 else:
-    s = str(datetime.datetime.today())
-    print('COTAÇÃO ATUAL'.center(51, '='))
-    print(f"Data: {s[8:10]}/{s[5:7]}/{s[:4]} {f'Hora: {s[11:16]}':>33}.")
+    getlink = re.get('http://worldtimeapi.org/api/timezone/America/Sao_Paulo')
+    dh = json.loads(getlink.text)['datetime']
+    data = f"{dh[8:10]}/{dh[5:7]}/{dh[:4]}"
+    hora = dh[11:16]
+    print(' COTAÇÃO ATUAL '.center(53, '='))
+    print(f'Data: {data}.  {f"Hora: {hora}":>33}.')
+    print('=' * 53)
     print()
     print(f'Dólar para Real:\n'
           f'[Topo]: 1 USD = {api["USDBRL"]["high"]} BRL.\n'
