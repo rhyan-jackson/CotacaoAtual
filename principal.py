ac9b25bf0ff62e5fac4095dfa4505db67f2f8990
Rhyan Jackson <rhyanjacksonmarinho@gmail.com>
2022-02-17 22:42:01 -0300

    Correção em principal.py e exclusão do .idea.
    Adição de data e hora no programa.

--- principal.py
+++ principal.py
@@ -1,6 +1,6 @@
 import json
 import requests as re
-
+import datetime
 try:
     api = re.get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL')
     api = json.loads(api.text)
@@ -8,6 +8,10 @@ except Exception as erro:
     print('Ocorreu um erro com sua requisição. Tente novamente.\n'
           f'Código de erro: {erro}.')
 else:
+    s = str(datetime.datetime.today())
+    print('COTAÇÃO ATUAL'.center(51, '='))
+    print(f"Data: {s[8:10]}/{s[5:7]}/{s[:4]} {f'Hora: {s[11:16]}':>33}.")
+    print()
     print(f'Dólar para Real:\n'
           f'[Topo]: 1 USD = {api["USDBRL"]["high"]} BRL.\n'
           f'[Piso]: 1 USD = {api["USDBRL"]["low"]} BRL.')
